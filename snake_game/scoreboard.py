f4092884c1235dab41eeb7f92e6bab6f3aba915c
russel-yella <67864019+russel-yella@users.noreply.github.com>
2026-03-06 12:42:19 +0100

    Update scoreboard.py

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -3,11 +3,13 @@ ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
 
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("text.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 265)
@@ -23,6 +25,8 @@ class Scoreboard(Turtle):
             self.high_score = self.score
         self.score = 0
         self.update_scoreboard()
+        with open("text.txt", "w") as file:
+            file.write(str(self.high_score))
 
     def increase_score(self):
         self.score += 1
